22c765adec8ded4c1995010d750ec55bb49dd088
amihut <amihut@EN610723>
2020-06-16 11:11:47 +0300

    handle signup form

--- src/components/HomeGuest.js
+++ src/components/HomeGuest.js
@@ -1,10 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Form, Button, Jumbotron, Container } from 'react-bootstrap'
 
 function HomeGuest() {
+  const [ email, setEmail ] = useState()
+  const [ password, setPassword ] = useState()
+
+  function handlePasswordMatch( e ) {
+    if ( e.target.value !== password ) {
+      alert( "Passwords must match" )
+    }
+  }
+
   function handleSubmit( e ) {
     e.preventDefault()
-    console.log( 'Submitting' )
+    console.log( `Email: ${ email }, password: ${ password }` )
   }
 
 
@@ -23,7 +32,7 @@ function HomeGuest() {
         <Form onSubmit={ e => handleSubmit( e ) } className='col-xs-12 col-lg-5'>
           <Form.Group controlId='formBasicEmail'>
             <Form.Label>Email address</Form.Label>
-            <Form.Control onChange={ e => console.log( e.target.value ) } type='email' placeholder='Enter email' />
+            <Form.Control onChange={ e => setEmail( e.target.value ) } type='email' placeholder='Enter email' />
             <Form.Text className='text-muted'>
               We'll never share your email with anyone else.
     </Form.Text>
@@ -31,11 +40,11 @@ function HomeGuest() {
 
           <Form.Group controlId='formBasicPassword'>
             <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Password' />
+            <Form.Control onChange={ e => setPassword( e.target.value ) } type='password' placeholder='Password' />
           </Form.Group>
           <Form.Group controlId='formRetypeBasicPassword'>
             <Form.Label>Re-enter Password</Form.Label>
-            <Form.Control type='password' placeholder='Re-eneter Password' />
+            <Form.Control onBlur={ e => handlePasswordMatch( e ) } type='password' placeholder='Re-eneter Password' />
           </Form.Group>
           <Button variant='outline-dark' type='submit'>
             Submit
